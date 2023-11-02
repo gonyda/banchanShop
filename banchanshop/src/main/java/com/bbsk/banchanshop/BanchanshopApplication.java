37fc7bb67ad9b34d4320da41dbb54cf35da21895
gony <bbsk3939@gmail.com>
2023-11-02 10:36:31 +0900

    init project

---
+++ banchanshop/src/main/java/com/bbsk/banchanshop/BanchanshopApplication.java
@@ -0,0 +1,13 @@
+package com.bbsk.banchanshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanchanshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanchanshopApplication.class, args);
+	}
+
+}
---
+++ banchanshop/src/test/java/com/bbsk/banchanshop/BanchanshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbsk.banchanshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanchanshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
