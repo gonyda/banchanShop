a3b8ed4d60ad5f633a10759a6602fd2d89987ce6
gony <bbsk3939@gmail.com>
2023-11-02 20:25:46 +0900

    객체생성 어노테이션 추가

--- banchanshop/src/main/java/com/bbsk/banchanshop/entity/SampleEntity.java
+++ banchanshop/src/main/java/com/bbsk/banchanshop/entity/SampleEntity.java
@@ -5,10 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
@@ -17,7 +18,7 @@ import lombok.ToString;
 @ToString
 @Builder
 @AllArgsConstructor
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SampleEntity {
 
 	@Id
