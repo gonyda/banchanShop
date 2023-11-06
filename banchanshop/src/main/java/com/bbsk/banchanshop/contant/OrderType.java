4126cd0bdb317f1b82f78cfbc1478d7bcbf95c36
gony <bbsk3939@gmail.com>
2023-11-07 00:59:10 +0900

    enum 추가, 주문종류 및 결제방법

---
+++ banchanshop/src/main/java/com/bbsk/banchanshop/contant/OrderType.java
@@ -0,0 +1,6 @@
+package com.bbsk.banchanshop.contant;
+
+public enum OrderType {
+	ORDER, // 일반주문
+	PREORDER // 예약주문 
+}
---
+++ banchanshop/src/main/java/com/bbsk/banchanshop/contant/PaymentType.java
@@ -0,0 +1,7 @@
+package com.bbsk.banchanshop.contant;
+
+public enum PaymentType {
+	CARD, // 신용카드
+	ACCOUNTTRANSFER, // 계좌이체
+	BANKTRANSFER // 무통장입금
+}
