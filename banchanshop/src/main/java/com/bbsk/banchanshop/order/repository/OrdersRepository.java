b9ab7660ba464cc1b07321421b100267d0d21f6c
gony <bbsk3939@gmail.com>
2023-11-09 15:08:12 +0900

    주문 조회 기능 테스트

--- banchanshop/src/main/java/com/bbsk/banchanshop/order/repository/OrdersRepository.java
+++ banchanshop/src/main/java/com/bbsk/banchanshop/order/repository/OrdersRepository.java
@@ -1,9 +1,13 @@
 package com.bbsk.banchanshop.order.repository;
 
+import com.bbsk.banchanshop.order.entity.OrderItemEntity;
 import com.bbsk.banchanshop.order.entity.OrdersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrdersRepository extends JpaRepository<OrdersEntity, Long> {
+import java.util.List;
 
+public interface OrdersRepository extends JpaRepository<OrdersEntity, Long> {
     OrdersEntity findTop1RecentOrderByUserUserIdOrderByOrderDateDesc(String userId);
+
+    List<OrdersEntity> findAllByUserUserId(String userId);
 }
